b4a4a74259b27e29fd85f5845102436b0247d2d7
Michelle Moon <michellemoon236@gmail.com>
2019-06-20 22:15:32 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,11 @@ class Post < ActiveRecord::Base
   def clickbait?
     # binding.pry 
     if self.title != nil
+<<<<<<< HEAD
       self.title.include?("Won't Believe") || self.title.include?("Secret") || self.title.include?("Top") || self.title.include?("Guess") ? true : errors.add(:title, "must be clickbait")
+=======
+      self.title.include?("Won't Believe") || self.title.include?("Secret") || self.title.include?("Top") || self.title.include?("Guess") ? errors.add(:title, "must be clickbait") : true
+>>>>>>> 9d37d2f479afc4a77b673c0619341a1b04c94c34
     end
   end
 end
